cbd51e822dde919099947a8787d5ed16afa737a8
farassri <100525104+farassri@users.noreply.github.com>
2023-10-13 13:33:52 +0800

    Create days5.java

---
+++ days5.java
@@ -0,0 +1,14 @@
+
+package perulangan5;
+
+public class Perulangan5 {
+
+   
+    public static void main(String[] argumen) {
+        for(int i = 1; i <= 20; i += 3){
+            System.out.println(i + "");
+        }
+       
+    }
+    
+}
