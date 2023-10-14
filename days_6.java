7ede3105acfe23dc5f4216e1e62a1c2f5f9a5267
farassri <100525104+farassri@users.noreply.github.com>
2023-10-14 21:29:07 +0800

    Create days_6.java

---
+++ days_6.java
@@ -0,0 +1,27 @@
+
+package pkg6_tipedata;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        String nama = "faras sri nurhayati";
+        int umur = 20;
+        double tinggi = 160;
+        char jenisKelamin = 'p'; 
+        float berat = 40;
+        boolean perilaku = true;
+        
+          
+        
+        System.out.println("nama " +nama);
+        System.out.println("umur : " + umur);
+        System.out.println("tinggi :" +tinggi);
+        System.out.println("jenis kelamin: " +jenisKelamin);
+        System.out.println("berat : "+berat);
+        System.out.println("perilaku :"+perilaku);
+                
+    }
+    
+}
