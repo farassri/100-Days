5143aa5d53dda3585e6c171e85bdf4fc3475bf02
farassri <100525104+farassri@users.noreply.github.com>
2023-10-15 22:02:25 +0800

    Create days_7.java

---
+++ days_7.java
@@ -0,0 +1,21 @@
+
+package logika_6;
+
+
+public class Logika {
+
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        
+        boolean hasilAND = a && b;//
+        boolean hasilOR = a || b;
+        boolean hasilNOT = !a;
+        
+        System.out.println("Hasil AND: " +hasilAND);
+        System.out.println("Hasil OR: " +hasilOR);
+        System.out.println("Hasil NOT:  "+hasilNOT);
+    }
+    
+}
