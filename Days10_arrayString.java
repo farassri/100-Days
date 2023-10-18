88ae8cd534e74c0d5e998f35aa19407aa76ce996
farassri <100525104+farassri@users.noreply.github.com>
2023-10-18 20:24:30 +0800

    Create Days10_arrayString.java

---
+++ Days10_arrayString.java
@@ -0,0 +1,10 @@
+package javaapplication103;
+
+
+public class JavaApplication103 {
+
+    
+    public static void main(String[] args) {
+      for(int i = 1;i < 1001; i++); 
+        System.out.println("SELAMAT DATANG DI TEKNIK INFORMATIKA");
+        System.out.println("SEMANGAT DAN JANGAN MENYERAH");
