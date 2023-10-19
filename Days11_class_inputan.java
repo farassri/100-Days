2a8745956ad2a91d52bab4bd2aaf092413977548
farassri <100525104+farassri@users.noreply.github.com>
2023-10-19 14:22:32 +0800

    Create Days11_class_inputan.java

---
+++ Days11_class_inputan.java
@@ -0,0 +1,17 @@
+
+package days11_classinputan;
+
+import java.util.Scanner;
+
+public class Days11_ClassInputan {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan teks: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Anda memasukkan: " + userInput);
+
+        
+    }
+}
