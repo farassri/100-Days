57b52aa9f5098ee7952eb2688a6945fb91f56584
farassri <100525104+farassri@users.noreply.github.com>
2023-10-20 20:14:52 +0800

    Create Days12_percabangan_if_elsa.java

---
+++ Days12_percabangan_if_elsa.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x > 5) {
+            System.out.println("x lebih besar dari 5");
+        } else {
+            System.out.println("x tidak lebih besar dari 5");
+        }
+    }
+}
