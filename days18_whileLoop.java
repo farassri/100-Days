aabb5a98c75389ee67f19cedc0f00856a310ee8c
farassri <100525104+farassri@users.noreply.github.com>
2023-10-26 16:06:55 +0800

    Create days18_whileLoop.java

---
+++ days18_whileLoop.java
@@ -0,0 +1,14 @@
+public class Days18_whileLoop {
+
+  
+    public static void main(String[] args) {
+       
+        int a = 1;
+        while (a <= 10) {
+            System.out.println(a);
+            a++;
+            
+        
+        }
+    }
+}
