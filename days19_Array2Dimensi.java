d47c26ce4224dc47d413f7ffc5d3c8163fb0b285
farassri <100525104+farassri@users.noreply.github.com>
2023-10-27 14:04:00 +0800

    Create days19_Array2Dimensi.java

---
+++ days19_Array2Dimensi.java
@@ -0,0 +1,16 @@
+public class Days19_Array2Dimensi {
+
+   
+    public static void main(String[] args) {
+       
+        String[][] huruf = {
+            {"a", "b", "c"},
+            {"D", "F", "G"},
+            {"i", "j", "k"},
+        };
+
+        // Menampilkan isi array pada indeks ke-[0][1] dan [2][0]
+        System.out.println(huruf[0][2]);
+        System.out.println(huruf[2][0]);  
+    }
+}
