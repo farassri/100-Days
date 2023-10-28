03e5ca60f409993821a77f2664eefe8c39df6e4e
farassri <100525104+farassri@users.noreply.github.com>
2023-10-28 08:01:25 +0800

    Create Days20_Array1dimensi.java

---
+++ Days20_Array1dimensi.java
@@ -0,0 +1,11 @@
+public class Days20 {
+
+    
+    public static void main(String[] args) {
+        int[] intArray = {1,2,3,4,5};
+        String arrayString = Arrays.toString(intArray);
+        
+        System.out.println(arrayString);
+    }
+    
+}
