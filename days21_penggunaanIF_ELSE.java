cd5b884bbec250781365137b1bf05412b4481e12
farassri <100525104+farassri@users.noreply.github.com>
2023-10-29 22:48:08 +0800

    Create days21_penggunaanIF_ELSE.java

---
+++ days21_penggunaanIF_ELSE.java
@@ -0,0 +1,13 @@
+public class Days_21 {
+
+   
+    public static void main(String[] args) {
+     int time = 20;
+     if (time <18) {
+         System.out.println("selamat siang");
+     }else {
+         System.out.println("selamat malam");
+    }
+    }
+    
+}
