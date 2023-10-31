908bc8bd65613fe5c851449588a00c6226bc621d
farassri <100525104+farassri@users.noreply.github.com>
2023-10-31 22:11:32 +0800

    Create days23_kalkulatorSederhana.java

---
+++ days23_kalkulatorSederhana.java
@@ -0,0 +1,21 @@
+public class Days23 {
+
+    
+    public static void main(String[] args) {
+        Scanner angka = new Scanner(System.in);
+        System.out.println("masukkan angka pertama: ");
+        double angka1 = angka.nextDouble();
+        System.out.println("masukkan angka kedua: ");
+        double angka2 = angka.nextDouble();
+        
+        System.out.println("penjumlahan :" + (angka1 + angka2));
+        System.out.println("pengurangan :" + (angka1 - angka2));
+        System.out.println("perkalian :" + (angka1 * angka2));
+        System.out.println("pembagian :" + (angka1 / angka2));
+        
+        
+        
+        
+    }
+    
+}
