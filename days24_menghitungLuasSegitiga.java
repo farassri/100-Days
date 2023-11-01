ebe3008bb3420efde05bdd00afdae74d28e0c8b7
farassri <100525104+farassri@users.noreply.github.com>
2023-11-01 23:14:51 +0800

    Create days24_menghitungLuasSegitiga.java

---
+++ days24_menghitungLuasSegitiga.java
@@ -0,0 +1,20 @@
+public class Days_24 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("menghitung luas segitiga");
+        System.out.println("------------------------");
+        
+        Scanner input = new Scanner (System.in);
+        System.out.println("masukkan nilai alas: ");
+        int a = input.nextInt();
+        System.out.println("masukkan nilai tinggi: ");
+        int b = input.nextInt();
+        
+        double l = (a*b)/2;
+        System.out.println("-----------------------------------------------------");
+        System.out.println("hasil perhitungan dari luas segitiga adalah ");
+    }
+    
+}
