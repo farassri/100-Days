18358cad6bd9a32d097b8bb5060c2fa01fbc1720
farassri <100525104+farassri@users.noreply.github.com>
2023-11-04 23:09:52 +0800

    Create days27_menggunakanTipeDataByteUntukPenjumlahan.java

---
+++ days27_menggunakanVariabelByteUntukPenjumlahan.java
@@ -0,0 +1,13 @@
+public class Days27 {
+
+    
+    public static void main(String[] args) {
+        byte a = 30;
+        byte b = 50;
+        byte c =(byte)(a+b);
+        
+        
+        System.out.println("nilai variabel c penjumlahannya adalah : " +c);
+    }
+    
+}
