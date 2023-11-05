3d386a4c5ce6612dd0092706293b4567c9910251
farassri <100525104+farassri@users.noreply.github.com>
2023-11-05 21:20:35 +0800

    Create days28_operasiAritmetika.java

---
+++ days28_operasiAritmetika.java
@@ -0,0 +1,13 @@
+public class days28 {
+
+   
+    public static void main(String[] args) {
+      int a = 5;
+      int b = 3;
+      
+        System.out.println(a+b);//hasil penjumlahan operasi aritmetika adalah 8
+        System.out.println(a-b);//hasil pengurangan operasi aritmetika adalah 2
+        System.out.println(a*b);//hasil pengurangan operasi aritmetika adalah 15
+        System.out.println(a/b);//hasil pengurangan operasi aritmetika adalah 1
+        System.out.println(a%b);//hasil pengurangan operasi aritmetika adalah 2
+        
