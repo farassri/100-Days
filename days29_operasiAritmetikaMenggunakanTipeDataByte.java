ad44abfd92860d28bbd0154fa7f1efb6f94347db
farassri <100525104+farassri@users.noreply.github.com>
2023-11-06 16:12:22 +0800

    Create days29_operasiAritmetikaMenggunakanTipeDataByte.java

---
+++ days29_operasiAritmetikaMenggunakanTipeDataByte.java
@@ -0,0 +1,18 @@
+public class Days29 {
+
+    
+    public static void main(String[] args) {
+        //operasi aritmetika menggunakan tipe data byte
+       byte a = 10;
+       byte b = 8;
+       
+       
+        System.out.println(a+b);//hasil penjumlahan operasi aritmetika menggunakan tipe data byte adalah 18
+        System.out.println(a-b);//hasil penjumlahan operasi aritmetika menggunakan tipe data byte adalah 2
+        System.out.println(a*b);//hasil penjumlahan operasi aritmetika menggunakan tipe data byte adalah 80
+        System.out.println(a/b);//hasil penjumlahan operasi aritmetika menggunakan tipe data byte adalah 1
+        System.out.println(a%b);//hasil penjumlahan operasi aritmetika menggunakan tipe data byte adalah 2
+      
+    }
+    
+}
