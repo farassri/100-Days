646ed40c53cb42547bb0e54ea5318cde2d68f5ce
farassri <100525104+farassri@users.noreply.github.com>
2023-11-09 20:42:08 +0800

    Create days32_MenentukanKelulusanMenggunakanIF.java

---
+++ days32_MenentukanKelulusanMenggunakanIF.java
@@ -0,0 +1,12 @@
+public class Days32 {
+
+    
+    public static void main(String[] args) {
+     int nilai = 80;
+     
+     if (nilai > 60){
+         System.out.println("Nilai = " +nilai+ " lulus ujian ");
+     }
+    }
+    
+}
