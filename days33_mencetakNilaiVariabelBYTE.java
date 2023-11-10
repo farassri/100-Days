99d93e87ab06aa033fe0797440b8e686afbb8427
farassri <100525104+farassri@users.noreply.github.com>
2023-11-10 18:42:22 +0800

    Create days33_mencetakNilaiVariabelBYTE.java

---
+++ days33_mencetakNilaiVariabelBYTE.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+       byte a = 10;
+       
+        System.out.println("=====BYTE=====");
+        System.out.println("nilai byte a = " + a);
+        System.out.println("nilai max  = "+Byte.MAX_VALUE);
+        System.out.println("nilai min  = "+Byte.MIN_VALUE);
+        System.out.println("besar byte = "+Byte.BYTES +" byte");
+        System.out.println("besar byte = "+Byte.SIZE +" bit");
+    }
+    
+}
