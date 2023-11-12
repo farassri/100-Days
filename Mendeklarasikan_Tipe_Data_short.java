1c5f8fded4aff77c28f192f247020a1f39ef02ff
farassri <100525104+farassri@users.noreply.github.com>
2023-11-12 03:49:18 -1100

    days35_mendeklarasikanTipeDataShort

---
+++ Mendeklarasikan_Tipe_Data_short.java
@@ -0,0 +1,12 @@
+package days35;
+
+public class Mendeklarasikan_Tipe_Data_short {
+
+    public static void main(String[] args) {
+        //Deklarasi variabel
+        short value = 2;
+        //Output
+        System.out.println("Nilai dari short = " + value);
+    }
+
+}
