e7b4009de2c56326a22b23d051db655b8b77fe57
farassri <100525104+farassri@users.noreply.github.com>
2023-11-14 23:36:06 +0800

    Create days37_MenampilkanKarakterMenggunakanTipeDataChart.java

---
+++ days37_MenampilkanKarakterMenggunakanTipeDataChart.java
@@ -0,0 +1,13 @@
+public class Days37 {
+
+    public static void main(String[] args) {
+        char nilai ='5';
+        char huruf ='A';
+        
+        
+        System.out.println("nilai yang akan muncul adalah: "+nilai);
+        System.out.println("huruf yang akan muncul adalah: "+huruf);         
+        
+    }
+    
+}
