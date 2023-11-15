9bffb64ec517a0b5bdce0ef7e783069e3769f9d8
farassri <100525104+farassri@users.noreply.github.com>
2023-11-15 22:01:37 +0800

    Create days38_mendeklarasikanTipeDataFloat.java

---
+++ days38_mendeklarasikanTipeDataFloat.java
@@ -0,0 +1,12 @@
+public class Days38 {
+
+    
+    public static void main(String[] args) {
+        float nilai1 = 154.22F;
+        float nilai2 = 150.16F;
+        
+        System.out.println("nilai dari float adalah: "+nilai1);
+        System.out.println("nilai dari float adalah: "+nilai2);
+    }
+    
+}
