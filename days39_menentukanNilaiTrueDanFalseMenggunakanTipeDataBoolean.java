2fc44e09cc266b7f45bf32c12b474f979040cddd
farassri <100525104+farassri@users.noreply.github.com>
2023-11-16 23:41:31 +0800

    Create days39_menentukanNilaiTrueDanFalseMenggunakanTipeDataBoolean.java

---
+++ days39_menentukanNilaiTrueDanFalseMenggunakanTipeDataBoolean.java
@@ -0,0 +1,15 @@
+public class Days39 {
+
+    
+    public static void main(String[] args) {
+       boolean nilai1;
+       boolean nilai2;
+        
+        nilai1 = true;
+        nilai2 = false;
+        
+        System.out.println(" nilai1 adalah : "+nilai1);
+        System.out.println(" nilai2 adalah : "+nilai2);   
+        
+   }
+}
