bba0b2032f5b99a856bf3b8d1f99156dc1647c9e
farassri <100525104+farassri@users.noreply.github.com>
2023-11-17 22:35:03 +0800

    Create days40_operatorPerbandinganMenggunakanTipeDataBoolean.java

---
+++ days40_operatorPerbandinganMenggunakanTipeDataBoolean.java
@@ -0,0 +1,19 @@
+ public class Days40 {
+
+    
+    public static void main(String[] args) {
+       boolean nilai1;
+       boolean nilai2;
+       
+       nilai1 = 10 < 20;
+       nilai2 = 10 > 20;
+       
+      if(nilai1){
+          System.out.println("nilai1 = "+nilai1);
+      }
+      if(nilai2){
+          System.out.println("nilai = "+nilai2);
+      }
+    }
+    
+}
