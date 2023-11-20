55310722c4ed4d1e9507e70f175a2c7cffe00c63
farassri <100525104+farassri@users.noreply.github.com>
2023-11-20 21:58:02 +0800

    Create days43_inputanMenggunakanTipeDataLong.java

---
+++ days43_inputanMenggunakanTipeDataLong.java
@@ -0,0 +1,11 @@
+public class days43 {
+    public static void main(String[] args) {
+        long bilanganLong; //mendeklarasikan variabel tipe data long
+        
+        Scanner input = new Scanner(System.in); //mengambil inputan dari keyboard
+        System.out.print("masukkan bilangan long : ");
+        bilanganLong = input.nextLong(); //mengambil inputan dari pengguna
+        System.out.println("bilangan yg dimasukkan: " +bilanganLong);
+    }
+            
+}
