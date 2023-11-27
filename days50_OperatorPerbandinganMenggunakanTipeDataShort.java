1f31aa137da6b9e32ffa611072ab21955b241b59
farassri <100525104+farassri@users.noreply.github.com>
2023-11-27 15:57:44 +0800

    Create days50_OperatorPerbandinganMenggunakanTipeDataShort.java

---
+++ days50_OperatorPerbandinganMenggunakanTipeDataShort.java
@@ -0,0 +1,20 @@
+public class days50 {
+    public static void main(String[] args) {
+        //deklarasi variabel short
+        short a = 25;
+        short b = 40;
+        
+        //A lebih besar dari nilai B maka hasilnya false
+        System.out.println(a > b); 
+         //A lebih kecil dari nilai B maka hasilnya true
+        System.out.println(a < b);
+        //A lebih kecil atau sama dengan nilai B maka hasilnya true
+        System.out.println(a <= b);
+        //A lebih besar atau sama dengan nilai B maka hasilnya false
+        System.out.println(a >= b);
+        //A sama dengan nilai B maka hasilnya false
+        System.out.println(a == b);
+        //A tidak sama dengan nilai B maka hasilnya true
+        System.out.println(a != b);
+    }
+}
