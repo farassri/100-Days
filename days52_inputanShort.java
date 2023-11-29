be0633ff54af2f57a0effb26b246e27359ff20fa
farassri <100525104+farassri@users.noreply.github.com>
2023-11-29 23:02:12 +0800

    Create days52_inputanShort.java

---
+++ days52_inputanShort.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class days52 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         
+         Short angka; //mendeklarasikan variabel short
+         System.out.println("masukkan angka short: ");
+         
+         angka = sc.nextShort();//membaca input dari sc
+         System.out.println("angka short yang saya masukkan adalah :" +angka);
+        
+        
+    }
+}
