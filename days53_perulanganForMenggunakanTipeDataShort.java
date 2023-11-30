b2c1de06a68986b43a1f58c69b8e84e173170fcb
farassri <100525104+farassri@users.noreply.github.com>
2023-11-30 20:45:32 +0800

    Create days53_perulanganForMenggunakanTipeDataShort.java

---
+++ days53_perulanganForMenggunakanTipeDataShort.java
@@ -0,0 +1,12 @@
+public class days53 {
+    public static void main(String[] args) {
+        short i = 0; //mendeklarasikan variabel short
+        
+        //perulangan for
+        for( i = 0; i <= 100; i++){
+            if(i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
