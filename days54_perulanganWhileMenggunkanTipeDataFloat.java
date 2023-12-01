8ac70e2d2bb22216aa4b12b08ed2d7399c8af69d
farassri <100525104+farassri@users.noreply.github.com>
2023-12-01 20:15:50 +0800

    Create days54_perulanganWhileMenggunkanTipeDataFloat.java

---
+++ days54_perulanganWhileMenggunkanTipeDataFloat.java
@@ -0,0 +1,12 @@
+public class days54 {
+    public static void main(String[] args) {
+       //mendeklarsikan variabel float
+        float i = 0.0f;
+        
+        //menggunakan perulangan while
+        while ( i <= 50.0f){
+            System.out.println(i);
+            i ++;
+        }      
+    }
+}
