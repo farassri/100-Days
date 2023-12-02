df1cc46000ad39b525363d4db9e4839648409419
farassri <100525104+farassri@users.noreply.github.com>
2023-12-02 23:19:32 +0800

    Create days55_mendeklarasikanArrayMenggunkanTipeDataFloat.java

---
+++ days55_mendeklarasikanArrayMenggunkanTipeDataFloat.java
@@ -0,0 +1,18 @@
+public class days55 {
+    public static void main(String[] args) {
+        //mendeklarasikan array float
+        float[] angka = new float[5];
+        
+        //mengisi nilai array
+        angka [0] = 11.2f;
+        angka [1] = 12.3f;
+        angka [2] = 13.4f;
+        angka [3] = 14.5f;
+        angka [4] = 15.6f;
+        
+        //menampilkan nilai array 
+        for (int i = 0; i < angka.length; i++){
+            System.out.println(angka[i]);
+        }
+    }
+}
