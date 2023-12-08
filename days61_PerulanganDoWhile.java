ccdd884d74023f5a9135c622726985c65a0700f1
farassri <100525104+farassri@users.noreply.github.com>
2023-12-08 22:29:13 +0800

    Create days61_PerulanganDoWhile.java

---
+++ days61_PerulanganDoWhile.java
@@ -0,0 +1,12 @@
+public class PerulanganDoWhileDays61 {
+    public static void main(String[] args) {
+        int i = 3;
+         
+        do{
+            System.out.println("nilai i : "+ i);
+            i++;
+            
+        }while (i <= 30);
+                
+    }
+}
