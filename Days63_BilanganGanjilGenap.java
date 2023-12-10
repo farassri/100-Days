9f7963fdc07dde5260e0f24e62e034d8030868fc
farassri <100525104+farassri@users.noreply.github.com>
2023-12-10 20:09:17 +0800

    Create Days63_BilanganGanjilGenap.java

---
+++ Days63_BilanganGanjilGenap.java
@@ -0,0 +1,17 @@
+package days63_bilanganganjilgenap;
+
+public class Days63_BilanganGanjilGenap {
+
+    public static void main(String[] args) {
+     
+    int number = 10;
+
+    if (number % 2 == 0) {
+      System.out.println(number + " bilangan genap");
+    } else {
+      System.out.println(number + " bilangan ganjil");
+    }
+  }
+}
+
+
