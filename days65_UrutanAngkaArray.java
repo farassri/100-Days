312512ac667d60c95ab99d7fbe0c50c3b6d133a2
farassri <100525104+farassri@users.noreply.github.com>
2023-12-12 21:46:42 +0800

    Create days65_UrutanAngkaArray.java

---
+++ days65_UrutanAngkaArray.java
@@ -0,0 +1,13 @@
+package javaapplication156;
+
+import java.util.Arrays;
+
+
+public class daysUrutanArray65 {
+    public static void main(String[] args) {
+        int [] angka = {20,10,5,15,25};
+         Arrays.sort(angka);
+         
+         System.out.println("angka terurut: "+Arrays.toString(angka));
+    }
+}
