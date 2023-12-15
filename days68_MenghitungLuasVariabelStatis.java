90545bd8208a7785f9ea8537915cf84b7607e847
farassri <100525104+farassri@users.noreply.github.com>
2023-12-15 23:15:26 +0800

    Create days68_MenghitungLuasVariabelStatis.java

---
+++ days68_MenghitungLuasVariabelStatis.java
@@ -0,0 +1,19 @@
+package days68_menghitungjumlahvariabelstatis;
+
+
+public class Days68_MenghitungJumlahVariabelStatis {
+
+    public static void main(String[] args) {
+       
+        int a = 1;
+        int b = 2;
+        String c = "Hello, world!";
+        int jumlahVariabel = 0;
+
+        jumlahVariabel++;
+        jumlahVariabel++;
+        jumlahVariabel++;
+
+        System.out.println("Jumlah variabel: " + jumlahVariabel);
+    }
+}
