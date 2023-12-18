83138def123562e2b63b0cc27edb10eb1e0af651
farassri <100525104+farassri@users.noreply.github.com>
2023-12-18 23:41:05 +0800

    Create days71_konversiTipeDataIntKeBoolean.java

---
+++ days71_konversiTipeDataIntKeBoolean.java
@@ -0,0 +1,13 @@
+package javaapplication157;
+
+public class days71_konversiIntkeBoolean {
+    public static void main(String[] args) {
+        //deklarasi variabel
+        int nilai = 20;
+        //konversi tipe data int ke boolean
+        boolean hasil = (nilai > 0);
+        //hasil konversi
+        System.out.println("nilai : "+nilai);
+        System.out.println("hasil konversi: "+hasil);
+    }
+}
