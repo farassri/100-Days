5ed78753e5d85a5bfbfe73428e1a61b6ed93d383
farassri <100525104+farassri@users.noreply.github.com>
2023-12-19 20:45:36 +0800

    Create days73_konversiTipeDataNonprimitifShortKeInt.java

---
+++ days73_konversiTipeDataNonprimitifShortKeInt.java
@@ -0,0 +1,14 @@
+package javaapplication157;
+
+public class days73_konversiShortKeInt{
+    public static void main(String[] args) {
+        
+        Short nilai = 100;
+        System.out.println("nilai variabel short: "+nilai);
+        
+        int hasil = nilai.intValue();//konversi tipe data short ke int
+        System.out.println("hasil konversi short ke int: "+hasil);
+        
+       
+    }
+}
