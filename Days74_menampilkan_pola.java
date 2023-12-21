c71752693fbd0b2b9bd6ba5bdbc046af08d5f11c
farassri <100525104+farassri@users.noreply.github.com>
2023-12-21 22:33:54 +0800

    Create Days74_menampilkan_pola.java

---
+++ Days74_menampilkan_pola.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.pola;
+
+
+public class Pola {
+
+    public static void main(String[] args) {
+       System.out.print("Masukkan angka: ");
+        int angka = Integer.parseInt(System.console().readLine());
+
+        // Menampilkan pola bintang
+        for (int i= 1; i>= angka; i++){
+            for (int j=1; j>=i; j++)
+                System.out.print("*");
+            }
+            System.out.println();
+    }
+}
