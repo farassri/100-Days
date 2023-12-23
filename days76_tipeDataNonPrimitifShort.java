6d8f9a3c2f39b38361f06b0a775c9abddb8e17e0
farassri <100525104+farassri@users.noreply.github.com>
2023-12-23 20:18:29 +0800

    Create days76_tipeDataNonPrimitifShort.java

---
+++ days76_tipeDataNonPrimitifShort.java
@@ -0,0 +1,14 @@
+package javaapplication157;
+
+
+public class days76_tipeDataNonPrimitifShort {
+    public static void main(String[] args) {
+        
+        Short nilai = 25;
+        //mencetak nilai variabel non primitif ketipe data short
+        System.out.println("nilai variabel short: "+nilai);
+        ///mengubah nilai variabel non primitif tipe data short
+        nilai = 30;
+        System.out.println("nilai variabel short yang diubah: "+nilai);
+    }
+}
