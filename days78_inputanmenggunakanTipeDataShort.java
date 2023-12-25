4da3c65e56edccffce784f2e783480246e926226
farassri <100525104+farassri@users.noreply.github.com>
2023-12-25 23:33:40 +0800

    Create days78_inputanmenggunakanTipeDataShort.java

---
+++ days78_inputanmenggunakanTipeDataShort.java
@@ -0,0 +1,15 @@
+package javaapplication157;
+
+import java.util.Scanner;
+
+public class days78 {
+    public static void main(String[] args) {
+     
+      Scanner sc = new Scanner(System.in); //deklarasi scanner
+      short bilangan ;//medeklarasikan variabel short
+       System.out.println("masukkan bilangan short: ");//membaca nilai dari input keyboard
+       bilangan = sc.nextShort();
+       
+        System.out.println("bilangan: "+bilangan);//mencetak nilai bilangan
+    }
+}
