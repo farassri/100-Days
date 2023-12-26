6bb6a94bc7dbb198f6acf16343fa2e3accd97ada
farassri <100525104+farassri@users.noreply.github.com>
2023-12-27 07:57:31 +0800

    Create Days80_perulanganD0WhileChar.java

---
+++ Days80_perulanganD0WhileChar.java
@@ -0,0 +1,15 @@
+package javaapplication157;
+
+public class days80_perulanganDoWhile {
+    public static void main(String[] args) {
+        //DEKLARASI VARIABEL CHAR
+        char huruf = 'A';
+       
+        do {
+            System.out.println("Huruf: "+huruf);//PERULANGAN DO WHILE
+            huruf ++;
+        } while (huruf <= 'Z');
+        
+    
+    }
+}
