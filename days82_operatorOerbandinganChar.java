6f450f736ee3d4540778671fdfe5329e1e5456cd
farassri <100525104+farassri@users.noreply.github.com>
2023-12-29 21:49:44 +0800

    Create days82_operatorOerbandinganChar.java

---
+++ days82_operatorOerbandinganChar.java
@@ -0,0 +1,16 @@
+package javaapplication157;
+
+
+public class days82_OperatorPerbandinganChar {
+    public static void main(String[] args) {
+        char a = 'A';//deklarasi variabel char
+        char b = 'B';
+        
+        System.out.println("A == B "+(a== b));//false
+        System.out.println("A != B  "+(a != b));//true
+        System.out.println("A < B  "+( a < b));//false
+        System.out.println("A > B  "+( a > b));//true
+        System.out.println("A >= B "+(a >= b));//false
+        System.out.println("A <= B "+ (a <= b));//true
+    }
+}
