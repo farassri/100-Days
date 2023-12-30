d40c46768dff9aa2dcf2ae7fc9d20563422222d7
farassri <100525104+farassri@users.noreply.github.com>
2023-12-30 22:12:54 +0800

    Create days83_mengonversiTipeDataCharKeInt.java

---
+++ days83_mengonversiTipeDataCharKeInt.java
@@ -0,0 +1,13 @@
+package javaapplication157;
+
+
+public class days83 {
+    public static void main(String[] args) {
+        //mengonversi nilai char ke integer
+        int i = (int)'A';
+        System.out.println("i = " + i);
+       
+        char c = (char)65;
+        System.out.println("c = "+ c);
+    }
+}
