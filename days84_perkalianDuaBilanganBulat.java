824a756f2bf12af22cd656c96eefc386e4202929
farassri <100525104+farassri@users.noreply.github.com>
2023-12-31 19:53:14 +0800

    Create days84_perkalianDuaBilanganBulat.java

---
+++ days84_perkalianDuaBilanganBulat.java
@@ -0,0 +1,12 @@
+package javaapplication157;
+
+
+public class days84 {
+    public static void main(String[] args) {
+        int a = 15;//DEKLARASI variabel
+        int b = 25;
+        int hasil = a*b;//perkalian
+        System.out.println("hasil perkalian " + a + " dan " + b + " adalah " + hasil);
+    }
+ 
+}
