87ac2cd6583787d73c3bdaefadbc6689fc6dc321
farassri <100525104+farassri@users.noreply.github.com>
2024-01-01 21:33:29 +0800

    Create days85_operatorPerkalian.java

---
+++ days85_operatorPerkalian.java
@@ -0,0 +1,18 @@
+ackage javaapplication157;
+
+public class days85 {
+    public static void main(String[] args) {
+        // Perkalian dua bilangan real
+        double a = 1.5;
+        double b = 2.5;
+        double hasil = a * b;
+        System.out.println("Hasil perkalian " + a + " dan " + b + " adalah " + hasil); // 3.75
+
+        // Perkalian bilangan bulat dengan bilangan real
+        int x = 10;
+        double y = 2.5;
+        double hasil2 = x * y;
+        System.out.println("Hasil perkalian " + x + " dan " + y + " adalah " + hasil2); // 25
+
+    }
+}
