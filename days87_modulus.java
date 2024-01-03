20f8be47217e4d431f25e0edd6726ce308de6712
farassri <100525104+farassri@users.noreply.github.com>
2024-01-03 21:45:00 +0800

    Create days87_modulus.java

---
+++ days87_modulus.java
@@ -0,0 +1,12 @@
+ackage javaapplication157;
+
+public class days87_NonprimitifModulusInt{
+    public static void main(String[] args) {
+     int pertama = 15;
+     int kedua = 5;
+     
+     int sisaHasilBagi = pertama % kedua;
+        System.out.println("sisa hasil bagi dari " + pertama + " dan " + kedua + " adalah " + sisaHasilBagi);
+        
+    }
+  }
