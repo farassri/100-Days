ffdc691741c5b7fa74fb34d8ec01099c6b43a5bd
farassri <100525104+farassri@users.noreply.github.com>
2024-01-05 18:50:41 +0800

    Create days89_menghitungNilaiKonstanta.java

---
+++ days89_menghitungNilaiKonstanta.java
@@ -0,0 +1,14 @@
+package javaapplication157;
+
+import java.util.Scanner;
+
+
+public class days89_konstanta {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("masukkan nilai konstanta: ");//membaca nilai konstanta
+        int konstanta = sc.nextInt();
+        int hasil = konstanta*2;//menghitung nilai konstanta
+        System.out.println("Hasil perhitungan: "+ hasil);//menampilkan hasil
+    }
+}
