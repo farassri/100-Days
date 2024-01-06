27fd5768a3e11863be362bc7c6d63bfcbd9bcaf7
farassri <100525104+farassri@users.noreply.github.com>
2024-01-06 22:48:25 +0800

    Create days90_d0WhileMenghitungAngkaNegatif.java

---
+++ days90_d0WhileMenghitungAngkaNegatif.java
@@ -0,0 +1,25 @@
+
+package javaapplication157;
+import java.util.Scanner;
+
+public class days90_DoWhileMenghitungAngkaNegatif {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        
+        int i = 0;
+        int jumlah = 0;
+        
+        do{
+            System.out.println("masukkan angka negatif: ");
+            int angka = sc.nextInt();
+            
+            if(angka <0 ){
+                jumlah += angka;
+            }
+            i++;
+        }while (i < 3);
+        System.out.println("jumlah angka negatif: "+jumlah);
+    }
+            
+}
+
