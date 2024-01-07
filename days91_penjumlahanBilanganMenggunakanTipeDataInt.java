50c0acdce137134cd58b686450d8aa90459a6517
farassri <100525104+farassri@users.noreply.github.com>
2024-01-07 21:40:51 +0800

    Create days91_penjumlahanBilanganMenggunakanTipeDataInt.java

---
+++ days91_penjumlahanBilanganMenggunakanTipeDataInt.java
@@ -0,0 +1,16 @@
+
+package javaapplication158;
+
+
+public class days91_PenjumlahanBilangan {
+
+    
+    public static void main(String[] args) {
+       int a = 15;
+       int b = 20;
+       
+       int hasil = a+b;
+        System.out.println(" hasil penjumlahan: "+hasil);
+    }
+    
+}
