7d02c15ce559b1a7e0ebe8ca6042f2b8591279d5
farassri <100525104+farassri@users.noreply.github.com>
2024-01-08 21:44:45 +0800

    Create days92_PemisahanJumlahBuah.java

---
+++ days92_PemisahanJumlahBuah.java
@@ -0,0 +1,18 @@
+
+package javaapplication158;
+
+
+public class days92_pemisahanJunlahBuah {
+    public static void main(String[] args) {
+      //Deklarasi Variabel
+      int jumlahBuah = 150;
+      int jumlahKemasanPerkotak = 10;
+      int jumlahKotak = 0;
+       
+      
+      jumlahKotak = jumlahBuah / jumlahKemasanPerkotak;//menghitung jumlah kotak
+        System.out.println("jumlah buah : "+jumlahBuah);
+        System.out.println("jumlah kemasan perkotak :"+jumlahKemasanPerkotak);
+        System.out.println("jumlah kotak : "+jumlahKotak);
+    }
+}
