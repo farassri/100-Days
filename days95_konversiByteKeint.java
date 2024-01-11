daf6590e8f0f10dd021e26c0a1aa5bc93969fa81
farassri <100525104+farassri@users.noreply.github.com>
2024-01-11 22:20:32 +0800

    Create days95_konversiByteKeint.java

---
+++ days95_konversiByteKeint.java
@@ -0,0 +1,14 @@
+
+package javaapplication160;
+
+
+public class days9_konversiByteKeInt {
+
+    
+    public static void main(String[] args) {
+        Byte a = 10;
+        int b = a.intValue();
+        System.out.println("a.intValue : "+b);
+    }
+    
+}
