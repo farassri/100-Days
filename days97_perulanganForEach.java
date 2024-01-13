e2b77e01291c565b90a5ff7bb1c175d800486d7a
farassri <100525104+farassri@users.noreply.github.com>
2024-01-13 22:05:03 +0800

    Create days97_perulanganForEach.java

---
+++ days97_perulanganForEach.java
@@ -0,0 +1,15 @@
+
+package javaapplication160;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class days97_perulanganForEach {
+    public static void main(String[] args) {
+        List<String> listNama = Arrays.asList("sinta","kia","linda","faras");
+        for(String nama : listNama){
+            System.out.println(nama);
+        }
+    }
+}
