19a77ac2d6d356f2d62d622c47777c54a85e6101
farassri <100525104+farassri@users.noreply.github.com>
2024-01-14 14:06:17 +0800

    Create day98_inputanShort.java

---
+++ day98_inputanShort.java
@@ -0,0 +1,15 @@
+
+package javaapplication158;
+
+import java.util.Scanner;
+
+
+public class day98_inputanShort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short a;
+        System.out.println("masukkan nilai short: ");
+        a = sc.nextShort();
+        System.out.println("bilangan short yg di masukkan: "+a);
+    }
+}
