ba92d99e05c03f5b40338eca579d79f94749c6b3
farassri <100525104+farassri@users.noreply.github.com>
2024-01-15 21:22:28 +0800

    Create days99_CharacterNonPrimitif.java

---
+++ days99_CharacterNonPrimitif.java
@@ -0,0 +1,11 @@
+package javaapplication158;
+
+public class day99 {
+    public static void main(String[] args) {
+        Character angka1 = 'A';
+        Character angka2 = 'B';
+        System.out.println("nilai karakter: "+angka1);
+        System.out.println("nilai karakter baru: "+angka2);
+        
+    }
+}
