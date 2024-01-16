03d8625f4d7dab3ef032ad83814483d7251ce900
farassri <100525104+farassri@users.noreply.github.com>
2024-01-16 21:48:40 +0800

    Create day100_KonversiIntkeDouble.java

---
+++ day100_KonversiIntkeDouble.java
@@ -0,0 +1,14 @@
+
+package javaapplication158;
+
+
+public class day100 {
+    public static void main(String[] args) {
+        int bilanganInt = 300;
+        double bilanganDouble;
+        
+        bilanganDouble = bilanganInt;
+        System.out.println("nilai bilangan int adalah "+bilanganInt);
+        System.out.println("nilai bilangan double adalah "+ bilanganDouble);
+    }
+}
